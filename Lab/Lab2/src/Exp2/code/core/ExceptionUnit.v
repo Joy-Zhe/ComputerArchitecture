049836b5c0ca4235011201a15ba481c467561847
Joy-Zhe <z13676788757@163.com>
2023-10-16 11:44:22 +0800

    init lab2

---
+++ Lab/Lab2/src/Exp2/code/core/ExceptionUnit.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+
+module ExceptionUnit(
+    input clk, rst,
+    input csr_rw_in,
+    input[1:0] csr_wsc_mode_in,
+    input csr_w_imm_mux,
+    input[11:0] csr_rw_addr_in,
+    input[31:0] csr_w_data_reg,
+    input[4:0] csr_w_data_imm,
+    output[31:0] csr_r_data_out,
+
+    input interrupt,
+    input illegal_inst,
+    input l_access_fault,
+    input s_access_fault,
+    input ecall_m,
+
+    input mret,
+
+    input[31:0] epc_cur,
+    input[31:0] epc_next,
+    output[31:0] PC_redirect,
+    output redirect_mux,
+
+    output reg_FD_flush, reg_DE_flush, reg_EM_flush, reg_MW_flush, 
+    output RegWrite_cancel
+);
+
+    reg[11:0] csr_raddr, csr_waddr;
+    reg[31:0] csr_wdata;
+    reg csr_w;
+    reg[1:0] csr_wsc;
+
+    wire[31:0] mstatus;
+
+    CSRRegs csr(.clk(clk),.rst(rst),.csr_w(csr_w),.raddr(csr_raddr),.waddr(csr_waddr),
+        .wdata(csr_wdata),.rdata(csr_r_data_out),.mstatus(mstatus),.csr_wsc_mode(csr_wsc));
+
+    //According to the diagram, design the Exception Unit
+    
+    
+    
+    assign PC_redirect = 0;
+    assign redirect_mux = 0;
+    assign RegWrite_cancel = 0;
+    assign reg_FD_flush = 0;
+    assign reg_DE_flush = 0;
+    assign reg_EM_flush = 0;
+    assign reg_MW_flush = 0;
+endmodule
